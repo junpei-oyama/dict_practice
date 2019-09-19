8e8bd18e52865c6efea31025d70a5cac51bba271
junpei-oyama <junpei.oyama.ai@gmail.com>
2019-09-19 15:10:38 +0900

    くだもの表示したよ

---
+++ fruit.py
@@ -0,0 +1,16 @@
+def main():
+    fruits_dict = {
+        'りんご': 'Apple',
+        'ぶどう': 'Grape',
+        'みかん': 'Orange',
+    }
+
+    print(fruits_dict['ぶどう'])
+
+    print(fruits_dict['みかん'])
+
+    print(fruits_dict['りんご'])
+
+
+if __name__ == '__main__':
+    main()
